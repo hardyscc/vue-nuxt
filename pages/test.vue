b8ce1f4c30795703322718b2ca4b134a9ee46a05
Hardys <hardyscc@gmail.com>
2017-12-02 22:21:43 +0800

    WIP

--- pages/test.vue
+++ pages/test.vue
@@ -1,3 +1,7 @@
 <template>
-  <div>Test</div>
+  <section class="section">
+    <div class="container has-text-centered">
+      <h2 class="subtitle">Testing Page</h2>
+    </div>
+  </section>
 </template>
